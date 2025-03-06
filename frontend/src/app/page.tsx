f47c570ec42d9298d2bd887db2001671599489bb
MuhammadFajrulfalaqIS <muhammadfajrulfalaqizzulfirdausyahsuryaprabandaru@mail.ugm.ac.id>
2025-03-06 21:28:09 +0700

    feat: add cover and button dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import './Dashboard.css';
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+  return (
+    <div className="dashboard">
+      <h1>GreenSortAI</h1>
+      <h2>Teknologi AI Untuk Daur Ulang yang Lebih Baik</h2>
+      <a href="/scan-sampah" className="start-button">Mulai Sekarang</a>
+    </div>
+  );
 }
