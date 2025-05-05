d503cc0de5c1a6b425b2109c7940485b51056992
Varick <Varick.zahir@gmail.com>
2025-05-05 23:24:52 +0700

    update: integrasi FE BE Perpustkaan, Detail

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,6 +1,43 @@
-import { useState } from 'react';
+"use client";
+
+import { useState, useEffect } from 'react';
+import { parse } from 'cookie';
+import Loading from './Loading';
+import LoginModal from './auth/LoginModal';
 
 export default function Hero() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState(true); // Use the User type
+    const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+    const toggleLogin = () => setIsLoginOpen(!isLoginOpen);
+    const closeLogin = () => setIsLoginOpen(false);
+
+    // Initialize user state from cookies
+    useEffect(() => {
+          const initializeUser = async () => {
+              try {
+                  const cookies = document.cookie;
+                  const parsedCookies = parse(cookies);
+                  const token = parsedCookies.auth_token;
+  
+                  if (token) {
+                     setUser(true);
+                  } else {
+                      setUser(false);
+                  }
+              } catch (error) {
+                  console.error("Error initializing user:", error);
+              } finally {
+                  setIsLoading(false);
+              }
+          };
+  
+          initializeUser();
+      }, []);
+  
+  if (isLoading) return <Loading />; // Show loading state while checking cookies
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-7xl mx-auto px-8 py-8 md:py-8 gap-6">
       {/* Left side - Text content */}
@@ -12,9 +49,18 @@ export default function Hero() {
           Teknologi AI untuk Daur Ulang yang Lebih Baik
         </p>
         <div className="pt-4">
-          <button className="bg-green-500 hover:bg-green-600 text-white font-medium py-3 px-8 rounded-md transition-colors duration-300 text-lg md:text-xl border-2 border-green-500 hover:border-green-600">
-            Mulai Sekarang!
-          </button>
+        <button
+          onClick={() => {
+            if (user) {
+              window.location.href = '/scan-sampah';
+            } else {
+              toggleLogin();
+            }
+          }}
+          className="bg-green-500 hover:bg-green-600 text-white font-medium py-3 px-8 rounded-md transition-colors duration-300 text-lg md:text-xl border-2 border-green-500 hover:border-green-600"
+        >
+          Mulai Sekarang!
+        </button>
         </div>
       </div>
       
@@ -31,6 +77,7 @@ export default function Hero() {
           </div>
         </div>
       </div>
+      <LoginModal isOpen={isLoginOpen} closeModal={closeLogin} />
     </div>
   );
 }
